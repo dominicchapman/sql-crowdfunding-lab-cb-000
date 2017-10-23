7a14827cac8002b48858d02ac69f92333b99d73a
Dominic Chapman <dominicchapman@me.com>
2017-10-23 22:37:59 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,3 +1,12 @@
 -- A project has a title, a category, a funding goal, a start date, and an end date.
 -- A user has a name and an age
 -- A pledge has an amount. It belongs to a user, and it also belongs to a project.
+
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal NUMERIC,
+  start_date TEXT,
+  end_date TEXT
+);
