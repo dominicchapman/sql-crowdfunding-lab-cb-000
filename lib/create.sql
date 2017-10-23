3dc8d596020a77ab42f62df4010d4ec72a3dc30f
Dominic Chapman <dominicchapman@me.com>
2017-10-23 22:40:07 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -10,3 +10,16 @@ CREATE TABLE projects (
   start_date TEXT,
   end_date TEXT
 );
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  amount NUMERIC,
+  user_id INTEGER,
+  project_id INTEGER
+);
