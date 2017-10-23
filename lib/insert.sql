f88dfafcd02e1fbc00aa36fa40339babad8713e8
Dominic Chapman <dominicchapman@me.com>
2017-10-23 22:49:45 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -19,3 +19,18 @@ INSERT INTO users (id, name, age) VALUES
 (18, 'Anna', 92),
 (19, 'Luke', 14),
 (20, 'Bob', 17);
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+(1, 'Storybook project', 'literature', 220.00, '2016-04-28', '2016-05-28'),
+(2, 'Artworks collection', 'arts', 40.00, '2016-04-30', '2016-05-30'),
+(3, 'Roger Rabbit Returns', 'literature', 40.00, '2017-02-20', '2017-03-20'),
+(4, 'Dachshund protection scheme', 'charity', 800.00, '2017-02-02', '2017-03-02'),
+(5, 'Party with the stars', 'charity', 400.00, '2015-08-16', '2015-09-16'),
+(6, 'Car collection', 'automobiles', 220.00, '2016-04-28', '2016-05-28'),
+(7, 'Cards', 'games', 2400.00, '2017-03-18', '2017-04-18'),
+(8, 'Diamonds are forever', 'movies', 880.00, '2017-01-15', '2017-02-15'),
+(9, 'Christina Aguilera collection', 'music', 10.00, '2014-04-24', '2014-05-24'),
+(10, 'Food for newbies', 'literature', 30.00, '2016-08-18', '2016-08-18');
+
+
+
