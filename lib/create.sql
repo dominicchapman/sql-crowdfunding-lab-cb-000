4d4694ffdccbcd1d4f6673c083445231579aa616
Dominic Chapman <dominicchapman@me.com>
2017-10-23 22:36:23 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,3 @@
+-- A project has a title, a category, a funding goal, a start date, and an end date.
+-- A user has a name and an age
+-- A pledge has an amount. It belongs to a user, and it also belongs to a project.
